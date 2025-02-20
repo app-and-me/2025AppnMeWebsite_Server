1ef2d8cf55eff96b39e83af482fae82b3a540aff
juyoung07 <yunju5090@gmail.com>
2025-02-20 23:28:53 +0900

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './ormconfig';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(typeORMConfig),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
