f5164ae97a004ddfda4b89b074c65702ea122fc4
juyoung07 <yunju5090@gmail.com>
2025-03-10 04:02:46 +0900

    [chore] : cors setting

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // CORS 설정
-  app.enableCors();
+  app.enableCors({
+    origin: [process.env.DOMAIN, process.env.DOMAIN2],
+    methods: 'GET, POST, DELETE, PUT, PATCH, HEAD',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
 
   await app.listen(process.env.PORT);
 }
