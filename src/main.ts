1d5b9a29d34c25400f33fd5fd7bf05e6fa51a817
juyoung07 <yunju5090@gmail.com>
2025-03-10 16:27:00 +0900

    [chore] : nestjs ssl

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('../localhost-key.pem'),
+    cert: fs.readFileSync('../localhost.pem'),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
 
   // CORS 설정
-  app.enableCors({
+  출처: https: app.enableCors({
     origin: '*',
     methods: 'GET, POST, DELETE, PUT, PATCH, HEAD',
     credentials: true,
